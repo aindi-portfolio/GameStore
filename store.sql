5f4a3f576954e22f55527410c59b644f4679e34e
aindi-portfolio <aindi.tsarni@gmail.com>
2025-07-14 16:54:46 -0400

    added SELECT setval('table_name_id-seq', (SELECT MAX(id) FROM table_name); under all tables in the store.sql so that additional infromation can be inserted into the tables without getting the ID error. Also changed the primary key types to SERIAL, because that was another issue that prevented the IDs from sequentially being increased as more information was added to the tables.

--- store.sql
+++ store.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS game;
 
 -- Create Game Table
 CREATE TABLE game (
-    game_id INT PRIMARY KEY,
+    game_id SERIAL PRIMARY KEY,
     game_title VARCHAR(255),
     quantity INT,
     price DECIMAL(5, 2)
@@ -17,32 +17,35 @@ CREATE TABLE game (
 DROP TABLE IF EXISTS action_figure;
 
 CREATE TABLE action_figure (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     action_figure_title VARCHAR,
     quantity INT,
     price DECIMAL(5,2)
 );
 
 \COPY action_figure FROM './data/action_figure.csv' WITH CSV HEADER;
+SELECT setval('action_figure_id_seq', (SELECT MAX(id) FROM action_figure));
 
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     employee_name VARCHAR,
     position VARCHAR,
     salary DECIMAL(7,2)
 );
 
 \COPY employee FROM './data/employee.csv' WITH CSV HEADER;
+SELECT setval('employee_id_seq', (SELECT MAX(id) FROM employee));
 
 DROP TABLE IF EXISTS poster;
 
 CREATE TABLE poster (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     poster_title VARCHAR,
     quantity INT,
     price DECIMAL(4,2)
 );
 
 \COPY poster FROM './data/poster.csv' WITH CSV HEADER;
+SELECT setval('poster_id_seq', (SELECT MAX(id) FROM poster));
